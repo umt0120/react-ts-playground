27eb6b6ab2644878e1489318eaaef2c470af6fff
UEMOTO Soichiro <uemoto.soichiro@ryobi.co.jp>
2023-08-09 11:39:12 +0900

    feat: 動画読み取り時の処理を実装

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { VideoUploader } from "./components/VideoUploader";
 export const App = () => {
   return (
     <div className="container">
-      <h1>Hello World.</h1>
       <VideoUploader />
     </div>
   );
--- src/components/VideoUploader.tsx
+++ src/components/VideoUploader.tsx
@@ -12,10 +12,11 @@ export const VideoUploader: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [duration, setDuration] = useState<number>(0);
   const [currentTime, setCurrentTime] = useState<number>(0);
-  const [rectangle, setRectangle] = useState<Rectangle>({ x: 50, y: 50, width: 100, height: 100 });
+  const rectangleRef = useRef<Rectangle>({ x: 50, y: 50, width: 100, height: 100 });
   const [isDragging, setIsDragging] = useState<boolean>(false);
   const [brightness, setBrightness] = useState<number>(0);
 
+  // ファイル入力時のコールバック
   const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (!file) return;
@@ -26,7 +27,15 @@ export const VideoUploader: React.FC = () => {
     if (!video) return;
 
     video.src = url;
-    video.play();
+    video.load();
+
+    video.onloadedmetadata = () => {
+      setDuration(video.duration);
+    };
+
+    video.ontimeupdate = () => {
+      setCurrentTime(video.currentTime);
+    };
   };
 
   const handlePlay = () => {
@@ -58,40 +67,32 @@ export const VideoUploader: React.FC = () => {
     const context = canvas.getContext("2d");
     if (!context) return;
 
-    video.addEventListener("loadedmetadata", function () {
-      setDuration(video.duration);
-    });
-
-    video.addEventListener("timeupdate", function () {
-      setCurrentTime(video.currentTime);
-    });
-
-    // video.addEventListener("play", function () {
-    //   const interval = setInterval(function () {
-    //     context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    //   }, 20);
-    //   return () => clearInterval(interval);
-    // });
+    const rect = canvas.getBoundingClientRect();
 
     const handleMouseDown = (event: MouseEvent) => {
-      console.log("mouse down")
+      const mouseX = event.clientX - rect.left;
+      const mouseY = event.clientY - rect.top;
+
       if (
-        event.clientX >= rectangle.x &&
-        event.clientX <= rectangle.x + rectangle.width &&
-        event.clientY >= rectangle.y &&
-        event.clientY <= rectangle.y + rectangle.height
+        mouseX >= rectangleRef.current.x &&
+        mouseX <= rectangleRef.current.x + rectangleRef.current.width &&
+        mouseY >= rectangleRef.current.y &&
+        mouseY <= rectangleRef.current.y + rectangleRef.current.height
       ) {
         setIsDragging(true);
       }
     };
 
     const handleMouseMove = (event: MouseEvent) => {
+      const mouseX = event.clientX - rect.left;
+      const mouseY = event.clientY - rect.top;
+
       if (isDragging) {
-        setRectangle({
-          ...rectangle,
-          x: event.clientX - rectangle.width / 2,
-          y: event.clientY - rectangle.height / 2,
-        });
+        rectangleRef.current = {
+          ...rectangleRef.current,
+          x: mouseX - rectangleRef.current.width / 2,
+          y: mouseY - rectangleRef.current.height / 2,
+        };
       }
     };
 
@@ -103,28 +104,23 @@ export const VideoUploader: React.FC = () => {
     canvas.addEventListener("mousemove", handleMouseMove);
     canvas.addEventListener("mouseup", handleMouseUp);
 
-    return () => {
-      canvas.removeEventListener("mousedown", handleMouseDown);
-      canvas.removeEventListener("mousemove", handleMouseMove);
-      canvas.removeEventListener("mouseup", handleMouseUp);
-    };
-  }, [rectangle, isDragging]);
-
-  useEffect(() => {
-    const video = videoRef.current;
-    const canvas = canvasRef.current;
-    if (!video || !canvas) return;
-
-    const context = canvas.getContext("2d");
-    if (!context) return;
-
     video.addEventListener("play", function () {
       const interval = setInterval(function () {
         context.drawImage(video, 0, 0, canvas.width, canvas.height);
         context.strokeStyle = "red";
-        context.strokeRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        context.strokeRect(
+          rectangleRef.current.x,
+          rectangleRef.current.y,
+          rectangleRef.current.width,
+          rectangleRef.current.height,
+        );
 
-        const imageData = context.getImageData(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        const imageData = context.getImageData(
+          rectangleRef.current.x,
+          rectangleRef.current.y,
+          rectangleRef.current.width,
+          rectangleRef.current.height,
+        );
         let totalBrightness = 0;
 
         for (let i = 0; i < imageData.data.length; i += 4) {
@@ -140,15 +136,27 @@ export const VideoUploader: React.FC = () => {
 
       return () => clearInterval(interval);
     });
-  }, [rectangle]);
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [isDragging]);
 
   return (
     <div>
+      {/* 画像入力 */}
       <input type="file" accept="video/*" onChange={handleUpload} />
+      {/* 再生・停止ボタン */}
       <button onClick={handlePlay}>Play</button>
       <button onClick={handlePause}>Pause</button>
+
+      {/* 現在フレームの輝度 */}
       <p>Brightness: {brightness}</p>
+
       <div style={{ width: "640px" }}>
+        {/* シークバー */}
         <input
           type="range"
           min="0"
@@ -157,16 +165,21 @@ export const VideoUploader: React.FC = () => {
           onChange={handleSeekChange}
           style={{ width: "100%" }}
         />
+
+        {/* 現在時刻 */}
         <p>
           Current Time: {Math.round(currentTime)} / {Math.round(duration)} seconds
         </p>
+        {/* シークバー目盛り */}
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           {Array.from({ length: Math.floor(duration / 10) + 1 }, (_, i) => (
             <span key={i}>{i * 10}</span>
           ))}
         </div>
       </div>
+      {/* 動画 */}
       <video ref={videoRef} style={{ display: "none" }} />
+      {/* フレームを描画するCanvas */}
       <canvas ref={canvasRef} width="640" height="480" />
     </div>
   );
